1b908e15f473ae83c4d1113a394c06371f858b98
luckylemon8 <133763365+luckylemon8@users.noreply.github.com>
2025-12-08 10:23:29 +0000

    logging in and logging out is now available (ui for login is done)

--- db.py
+++ db.py
@@ -18,3 +18,23 @@ def GetAllMovies():
     movies = db.execute("SELECT * FROM Movies").fetchall()
     db.close()
     return movies
+
+
+def CheckLogin(username, password):
+
+    db = GetDB()
+
+    # Ask the database for a single user matching the provided name
+    user = db.execute(
+        "SELECT * FROM Users WHERE username=? COLLATE NOCASE", (username,)
+    ).fetchone()
+
+    # Do they exist?
+    if user is not None:
+        # OK they exist, is their password correct
+        if check_password_hash(user["password"], password):
+            # They got it right, return their details
+            return user
+
+    # If we get here, the username or password failed.
+    return None
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect
 import db
 
 app = Flask(__name__)
@@ -8,6 +8,31 @@ app.secret_key = "revmovies"
 @app.route("/")
 def Home():
     return render_template("base.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def Login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        # Did they provide good details
+        user = db.CheckLogin(username, password)
+        if user:
+            # Yes! Save their username and id then
+            session["id"] = user["id"]
+            session["username"] = user["username"]
+
+            # Send them back to the homepage
+            return redirect("/")
+
+    return render_template("login.html")
+
+
+@app.route("/logout")
+def Logout():
+    session.clear()
+    return redirect("/")
 
 
 @app.route("/movie_list")
